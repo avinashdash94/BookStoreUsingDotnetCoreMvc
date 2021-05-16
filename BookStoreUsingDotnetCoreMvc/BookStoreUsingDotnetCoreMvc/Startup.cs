45fbbfb9cdaf325e03acbc7698891d1c9e2fd6c8
Avinash Dash <20avinashdash@gmail.com>
2021-05-16 16:00:59 +0530

    Added dependancy for Mvc and created default home controller

--- BookStoreUsingDotnetCoreMvc/BookStoreUsingDotnetCoreMvc/Startup.cs
+++ BookStoreUsingDotnetCoreMvc/BookStoreUsingDotnetCoreMvc/Startup.cs
@@ -16,6 +16,8 @@ namespace BookStoreUsingDotnetCoreMvc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,25 @@ namespace BookStoreUsingDotnetCoreMvc
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) => {
+            //    await context.Response.WriteAsync("Hello from First Midd");
+            //     next();
+            //    await context.Response.WriteAsync("Hello from First Midd-2");
+            //});
+
+           
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    //await context.Response.WriteAsync("Hello World!");
+                //});
             });
+
+           
         }
     }
 }
